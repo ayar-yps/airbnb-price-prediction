93b09beeb02aa1df3279d318d38e97a88c9ea2db
ayar <ayar.yps@gmail.com>
2024-11-25 18:21:34 -0400

    fix: update message to correctly execute the test_predict.py script

--- build-and-local-deploy.sh
+++ build-and-local-deploy.sh
@@ -6,5 +6,5 @@ docker build -t airbnb-price-prediction .
 echo "************************"
 echo "DEPLOY FOR LOCAL TESTING"
 echo "************************"
-echo ">> After running successfully, execute: python src/test_predict.py --host localhost:9696"
+echo ">> After running successfully, execute: python tests/test_predict.py --host localhost:9696"
 docker run -it --rm -p 9696:9696 airbnb-price-prediction
