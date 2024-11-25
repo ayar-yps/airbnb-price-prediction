3c0a649adfdafc7bfbca149452c318feb3e214bf
ayar <ayar.yps@gmail.com>
2024-11-25 17:13:34 -0400

    feat: prepare bash script to build and deploy locally for testing

---
+++ build-and-local-deploy.sh
@@ -0,0 +1,10 @@
+echo "******************"
+echo "BUILD CUSTOM IMAGE"
+echo "******************"
+docker build -t airbnb-price-prediction .
+
+echo "************************"
+echo "DEPLOY FOR LOCAL TESTING"
+echo "************************"
+echo ">> After running successfully, execute: python src/test_predict.py --host localhost:9696"
+docker run -it --rm -p 9696:9696 airbnb-price-prediction
